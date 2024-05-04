dccda23f96bdac936a1fc0b5799a6424da6f888c
syihwamoza <mozakastella26@gmail.com>
2024-05-04 21:41:42 +0700

    Memindahkan arus ke node berikutnya

--- 0111_Doublelinkedlist/0111_Doublelinkedlist.cpp
+++ 0111_Doublelinkedlist/0111_Doublelinkedlist.cpp
@@ -46,7 +46,7 @@ void addNode()
         while (current != NULL && current->noMhs < newNode->noMhs)
         {                       // step 1.c: traverse the lst to find the correct position
             previous = current; // step 1.d: move the previous to the current node
-            current = current->
+            current = current->next; // step 1.e: move the current to the next node
         }
     }
 }
