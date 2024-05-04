93624cb72685d07d27852c762c8972eb00b97146
syihwamoza <mozakastella26@gmail.com>
2024-05-05 00:04:57 +0700

    Menambahkan implementasi fungsi listEmpty() untuk memeriksa apakah linked list kosong

--- 0111_Doublelinkedlist/0111_Doublelinkedlist.cpp
+++ 0111_Doublelinkedlist/0111_Doublelinkedlist.cpp
@@ -130,5 +130,11 @@ void deleteNode()
     }
 
     // Release the memory  of the node markes as current
+    delete current;
+    cout << "\x1b[32mRecord with roll number" << rollNo << "deleted\x1b[0m" << endl;
+}
 
+bool listEmpty()
+{
+    return (START == NULL);
 }
