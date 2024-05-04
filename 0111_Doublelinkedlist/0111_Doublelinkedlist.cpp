44eaf491e5b44949c8be2bc8b4b6241363d5c0cd
syihwamoza <mozakastella26@gmail.com>
2024-05-04 23:26:26 +0700

    Buat bidang sebelumnya dari titik node saat ini ke node baru

--- 0111_Doublelinkedlist/0111_Doublelinkedlist.cpp
+++ 0111_Doublelinkedlist/0111_Doublelinkedlist.cpp
@@ -51,6 +51,11 @@ void addNode()
 
         newNode->next = current; // step 4: Make the next field of the new node point to current.
         newNode->prev = previous; // step 5: Make the previous field of the new node point to previous.
+
+        if (current != NULL)
+        {
+            current->prev = newNode; // step 6: Make the prvious field of the curret node point to the new node
+        }
     }
 }
 
