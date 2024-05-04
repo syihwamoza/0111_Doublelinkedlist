f5bea960214160a5d050ee70f7acbd6f6307c359
syihwamoza <mozakastella26@gmail.com>
2024-05-04 23:46:45 +0700

    mulai dari node pertama

--- 0111_Doublelinkedlist/0111_Doublelinkedlist.cpp
+++ 0111_Doublelinkedlist/0111_Doublelinkedlist.cpp
@@ -88,4 +88,12 @@ void deleteNode()
 
     cout << "\nEnter the roll number of the student whose record is to be deleted: ";
     cin >> rollNo; // step 3: get the roll number to be deleted 
+
+    if (START == NULL)
+    {
+        cout << "List is empty" << endl;
+        return;
+    }
+
+    current = START; // step 1 : start from the first node
 }
