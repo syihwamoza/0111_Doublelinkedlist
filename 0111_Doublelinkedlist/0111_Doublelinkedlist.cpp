7706fc4b77c9c6639ff9e363979443a029c0239c
syihwamoza <mozakastella26@gmail.com>
2024-05-04 23:21:49 +0700

    Buat bidang sebelumnya dari titik node baru ke sebelumnya

--- 0111_Doublelinkedlist/0111_Doublelinkedlist.cpp
+++ 0111_Doublelinkedlist/0111_Doublelinkedlist.cpp
@@ -48,6 +48,9 @@ void addNode()
             previous = current; // step 1.d: move the previous to the current node
             current = current->next; // step 1.e: move the current to the next node
         }
+
+        newNode->next = current; // step 4: Make the next field of the new node point to current.
+        newNode->prev = previous; // step 5: Make the previous field of the new node point to previous.
     }
 }
 
