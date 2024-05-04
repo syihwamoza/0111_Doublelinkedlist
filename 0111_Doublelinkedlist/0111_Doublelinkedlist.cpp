000cc667e9f9a6b545baa15dbd9a72308dfdb7e4
syihwamoza <mozakastella26@gmail.com>
2024-05-04 21:05:56 +0700

    memasukkan node baru

--- 0111_Doublelinkedlist/0111_Doublelinkedlist.cpp
+++ 0111_Doublelinkedlist/0111_Doublelinkedlist.cpp
@@ -16,6 +16,10 @@ void addNode()
 	Node* newNode = new Node(); // step 1: create a new code
 	cout << "\nEnter the roll number of the student:";
 	cin >> newNode->noMhs; // Assign value to the data field of the new node
+	cout << "\nEnter the name of the student";
+	cin >> newNode->name; //Assign valur to the data field of the new node
+
+	//Insert the new node in the list
 }
 
 
