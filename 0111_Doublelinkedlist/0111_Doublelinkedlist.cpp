012e335ad0bf0a69510704f5af596ada45fe3f0d
syihwamoza <mozakastella26@gmail.com>
2024-05-04 21:40:30 +0700

    Memindahkan yang sebelumnya ke node saat ini

--- 0111_Doublelinkedlist/0111_Doublelinkedlist.cpp
+++ 0111_Doublelinkedlist/0111_Doublelinkedlist.cpp
@@ -44,8 +44,9 @@ void addNode()
         Node* previous = NULL; // step 1.b: previous node is NULL initially
 
         while (current != NULL && current->noMhs < newNode->noMhs)
-        {
-            previous = current; // step 1.c: traverse the lst to find the correct position
+        {                       // step 1.c: traverse the lst to find the correct position
+            previous = current; // step 1.d: move the previous to the current node
+            current = current->
         }
     }
 }
